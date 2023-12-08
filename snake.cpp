4ea0804d30c0305622859bb00e77325df993117a
Mufid Bahaudin Nugroho <141691172+mufidbahaudin@users.noreply.github.com>
2023-12-08 14:41:11 +0700

    Add files via upload

---
+++ snake.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+const int width = 48;
+const int height = 24;
+
+bool gameOver;
+bool gameActive;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup()
+{
+    gameActive = false;
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        tailX[i] = 0;
+        tailY[i] = 0;
+    }
+}
+
+void Draw()
+{
+    system("cls");
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "o";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else
+            {
+                bool printTail = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "O";
+                        printTail = true;
+                    }
+                }
+                if (!printTail)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    cout << "Score:" << score << endl;
+	cout << "\033[1;31mNOTICE! : Sorry, this game has a glitch\033[0m" << endl;
+}
+
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            if (gameActive)
+                gameOver = true;
+            break;
+        case 72:
+            dir = UP;
+            break;
+        case 80:
+            dir = DOWN;
+            break;
+        case 75:
+            dir = LEFT;
+            break;
+        case 77:
+            dir = RIGHT;
+            break;
+        }
+    }
+}
+
+void Move()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x >= width) x = 0;
+    else if (x < 0) x = width - 1;
+
+    if (y >= height) y = 0;
+    else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+        {
+            gameOver = true;
+            Setup(); 
+        }
+    }
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+void Play()
+{
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Move();
+        Sleep(1); 
+    }
+}
+
+void Exit()
+{
+    cout << "=====Good Bye=======" << endl;
+    gameActive = false;  
+}
+
+int main()
+{
+    char choice;
+
+    do
+    {
+        cout << "===== Snake Game =====" << endl;
+        cout << "1. Play" << endl;
+        cout << "2. Exit" << endl;
+        cout << "Select: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            gameActive = true;
+            Setup();
+            Play();
+            break;
+        case '2':
+            Exit();
+            break;
+        default:
+            cout << "\033[1;31m Error This Game \033[0m"  << endl; // eror keika selain angka pilihan 1 dan 2
+            break;
+        }
+
+    } while (gameActive);
+
+    return 0;
+}
+
